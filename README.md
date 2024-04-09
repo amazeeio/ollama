e75b2411d2d7927794e16215bfd0bd08a5174ae3
Marco Cadetg <marco.cadetg@amazee.io>
2024-04-09 17:07:38 +0200

    initial commit for ollama container

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ollama
+
 lagoon ollama
---
+++ curl-example.sh
@@ -0,0 +1,4 @@
+curl http://localhost:11435/api/embeddings -d '{
+  "model": "mxbai-embed-large",
+  "prompt": "Llamas are members of the camelid family"
+}'
