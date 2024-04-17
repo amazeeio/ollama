fd8597b4eb1badf3ff9862894aa5bd17b9a07867
Marco Cadetg <marco.cadetg@amazee.io>
2024-04-17 15:22:40 +0200

    open embeddings api

--- curl-example.sh
+++ curl-example.sh
@@ -2,3 +2,12 @@ curl http://localhost:11435/api/embeddings -d '{
   "model": "mxbai-embed-large",
   "prompt": "Llamas are members of the camelid family"
 }'
+
+curl https://nginx.main.ollama.test1.amazee.io/api/embeddings -d '{
+  "model": "mxbai-embed-large",
+  "prompt": "Llamas are members of the camelid family"
+}'
+
+curl http://localhost:11434/api/pull -d '{
+  "name": "all-minilm"
+}'
