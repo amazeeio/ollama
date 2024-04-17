b2dc63cc6b912b7e6a4a78afa229bc8228ac6cfb
Marco Cadetg <marco.cadetg@amazee.io>
2024-04-17 11:22:46 +0200

    update nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Ensure the directory exists and has correct permissions
+mkdir -p /etc/nginx
+chown nginx:nginx /etc/nginx
+
 # Generate the .htpasswd file from environment variables
 htpasswd -cb /etc/nginx/.htpasswd "$HT_USER" "$HT_PASS"
 
