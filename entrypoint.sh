6c5c561751a3bdd0df72b31e2b10acfebafda49e
Marco Cadetg <marco.cadetg@amazee.io>
2024-04-17 11:44:08 +0200

    make htpasswd work

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@
 mkdir -p /etc/nginx/conf.d
 
 # Generate the .htpasswd file from environment variables
+# check if HT_USER and HT_PASS are set
+if [ -z "$HT_USER" ] || [ -z "$HT_PASS" ]; then
+    echo "Warning: No HT_USER and/or HT_PASS set, using defaults"
+    HT_USER="lagoon_admin"
+    HT_PASS="lagoon_admin_pw"
+fi
 htpasswd -cb /etc/nginx/conf.d/.htpasswd "$HT_USER" "$HT_PASS"
 
 # Start NGINX in the foreground
