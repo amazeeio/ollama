46eba4cecbf6d84d9ddd1d09a624e407506928e4
Marco Cadetg <marco.cadetg@amazee.io>
2024-04-17 11:27:35 +0200

    another try on nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Ensure the directory exists and has correct permissions
-mkdir -p /etc/nginx
-chown nginx:nginx /etc/nginx
+# Ensure the configuration directory exists (though it should already)
+mkdir -p /etc/nginx/conf.d
 
 # Generate the .htpasswd file from environment variables
-htpasswd -cb /etc/nginx/.htpasswd "$HT_USER" "$HT_PASS"
+htpasswd -cb /etc/nginx/conf.d/.htpasswd "$HT_USER" "$HT_PASS"
 
 # Start NGINX in the foreground
 nginx -g 'daemon off;'
